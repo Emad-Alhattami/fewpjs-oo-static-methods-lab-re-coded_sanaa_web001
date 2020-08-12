7b1c4d4e720cdb3b092f1be946133ecd899268b9
Emad Alhattami <emad774373827@gmail.com>
2020-08-12 22:20:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,31 +1,32 @@
 class Formatter {
-  //add static methods here
-  class Formatter {
-  static capitalize( string ) {
-    return string.charAt( 0 ).toUpperCase() + string.slice( 1 )
-  }
-
-  static sanitize( string ) {
-    return string.replace( /[^A-Za-z0-9 '-]/g, '' )
-  }
-
-  static titleize( sentence ) {
-    let exceptions = [ 'the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from' ]
-    let result = [];
-    let arrayOfWords = sentence.split( " " )
-    for ( let n = 0; n < arrayOfWords.length; n++ ) {
-      if ( n == 0 ) {
-        result.push( this.capitalize( arrayOfWords[ n ] ) )
-      } else {
-        if ( exceptions.includes( arrayOfWords[ n ] ) ) {
-          result.push( arrayOfWords[ n ] )
-        } else {
-          result.push( this.capitalize( arrayOfWords[ n ] ) )
-        }
-      }
-
+  static capitalize(sentence){
+      let a= sentence.toUpperCase().charAt(0);
+      return a+sentence.substr(1);
     }
-    return result.join( " " );
+    static sanitize(sentence){
+  
+      let a = sentence.replace(/[&@^!\/\\#,+()$~%.":*?<>{}]/g, "");
+      return a;
+  
+    }
+    static titleize(sentence){
+      let wordArray = sentence.split(' ');
+      let superWord="";
+          for (let i = 0; i < wordArray.length; i++) {
+            if (i==0)
+            superWord += Formatter.capitalize(wordArray[i])+' ';
+          else  if( wordArray[i]=="the"||  wordArray[i]=="a"|| wordArray[i]== "an"||  wordArray[i]=="but"||  wordArray[i]=="of"|| wordArray[i]== "and"|| wordArray[i]== "for"|| wordArray[i]== "at"||  wordArray[i]=="by"|| wordArray[i]== "from")
+            superWord+=wordArray[i]+' ';
+  
+            else
+          {  if(i+1==wordArray.length)
+            superWord += Formatter.capitalize(wordArray[i]);
+              else
+              superWord += Formatter.capitalize(wordArray[i])+' ';}
+  
+              // superWord += wordArray[i].charAt(0).toUpperCase();
+          }
+          return superWord;
+  
+    }
   }
-}
-}
